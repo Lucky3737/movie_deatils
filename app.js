9866ba286faae3bcf522d508e58084b26d48a487
sagarchennuri <chennurisagar321@gmail.com>
2022-12-06 03:51:13 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const path = require("path");
+const app = express();
+
+const dbPath = path.join(__dirname, "moviesData.db");
+
+const db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+app.get("/movies/", async (request, response) => {
+  let movieName = `
+    select 
+    movie_name
+    from 
+    movie
+    group by
+    movie_name
+    `;
+  let names = await db.all(movieName);
+  response.send(names);
+});
+# movie_deatils
